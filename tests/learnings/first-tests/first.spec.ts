120b0f27f4f8ff05a88e5d8feeb3713f2160d34d
Andrea Murarikova (EIT) <Andrea.Murarikova@embedit.com>
2025-11-21 17:24:13 +0100

    New exercise project

--- tests/learnings/first-tests/first.spec.ts
+++ tests/learnings/first-tests/first.spec.ts
@@ -9,3 +9,29 @@ test("First Test", async ({ page }) => {
   await page.locator("#password").fill("Playwright321!");
   await page.locator('[type="submit"]').click();
 });
+
+/*
+V tomto asistovaném cvičení vytvoříme nový test pro založení nového projektu v Pmtool aplikaci.
+Náš cíl je:
+  Přihlásit se do Pmtoolu.
+  Otevřít menu Projects.
+  Kliknout na tlačítko Add Project.
+  Vyplnit povinná pole.
+  Uložit nový projekt.
+
+Budeme potřebovat nové Page Objecty:
+  ProjectsPage
+  CreateNewProjectModal
+  ProjectTasksPage
+
+Využijeme také knihovnu Faker.js pro generování dat.
+
+Vytvoříme pro naše cvičení branch: exercise_assisted_pages
+
+Části cvičení
+  1. Vytvoření PO
+  2. Vytvoření selektorů a metod v PO
+  3. Úprava stávajících PO
+  4. Vytvoření testu
+
+*/
